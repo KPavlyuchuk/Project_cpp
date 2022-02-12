a8bcfa4b8f9c203bb63b5df5ce51430502eb77a9
Kirill Pavlyuchuk <kirillpavlyuchuk@gmail.com>
2022-02-12 11:21:06 +0300

    Add tests

--- tests.c
+++ tests.c
@@ -1,13 +1,46 @@
 #include "stack.h"
-
+#define N 10
 
 int main() {
+	printf("Test 1: Create\n");
+	stack * s;
+	if((s = create_stack(-1)) != NULL)
+		printf("test 1.1 failed\n");
+	else 
+		printf("test 1.1 passed\n");
+	////
+	if ((s = create_stack(N)) == NULL)
+		printf("test 1.2 failed\n");
+	else 
+		printf("test 1.2 passed\n");
+	////
+	printf("Test 2: Push\n");
 	int i = 0;
-	stack * s = create_stack(10);
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < N; i++)
 		push(s,i);
-	for (i = 0; i < 10; i++)
-		printf("%d\n", pop(s));
+	push(s, 10);
+		printf("test 2.1 passed\n");
+	for (i = 0; i < N; i++) {
+		int j = pop(s);
+		if (j == -1)
+			printf("test 2.2 failed\n");
+		else 
+			printf("test 2.2 passed\n");
+		printf("%d\n", j);
+	}
+	push(NULL, 1);
+		printf("test 2.3 passed\n");
+	/////
+	printf("Test 3: Pop\n");
+	if (pop(s) != -1)
+		printf("test 3.1 failed\n");
+	else 
+		printf("test 3.1 passed\n");
+	pop(NULL);
+		printf("test 3.2 passed\n");
+	/////
+	printf("Test 4: Remove\n");
 	remove_stack(s);
+	printf("test 4.1 passed\n");
 	return 0;
 }
